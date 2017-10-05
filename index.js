340ef6f3aa6cde5ca239eaf85561972821a18993
Jessica Leaky <jessicaleaky@gmail.com>
2017-10-05 11:14:09 +0700

    Complete Level 1

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const numberC = 0.02
 console.log(numberA);
 console.log(numberB);
 console.log(numberC);
+
+// LEVEL 1
+
+console.log(1+2);
+console.log(10-2);
+console.log(2*2);
+console.log(6/3);
+console.log(15%2);
